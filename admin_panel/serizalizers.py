5095fe6d0664cfab4d368c73496e74c724967917
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-05-25 11:38:05 +0500

    WebSite API full

--- admin_panel/serizalizers.py
+++ admin_panel/serizalizers.py
@@ -63,20 +63,20 @@ class FlowersBaseAllSerializers(serializers.ModelSerializer):
 class FlowersBaseCruderializers(serializers.ModelSerializer):
     # id_category = CategoriyaAllSerializers(read_only=True)
     # id_sub_category = SubCategoriyaAllSerializers(read_only=True)
-    img = serializers.ListField(
-        child=serializers.ImageField(allow_empty_file=False, use_url=False),
-        write_only=True
-    )
+    # img = serializers.ListField(
+    #     child=serializers.ImageField(allow_empty_file=False, use_url=False),
+    #     write_only=True
+    # )
     class Meta:
         model = Flowers
-        fields = ['id','name','cotent','rank','price','like','iye','id_category','id_sub_category','img',]
+        fields = ['id','name','cotent','rank','price','like','iye','id_category','id_sub_category',]
     def create(self, validated_data):
-        img = validated_data.pop('img')
+        # img = validated_data.pop('img')
         flowers = Flowers.objects.create(**validated_data)
-        print(img)
-        for item in img:
-            images = FlowersImages.objects.create(id_flowers=flowers,img=item)
-            images.save()
+        # print(img)
+        # for item in img:
+        #     images = FlowersImages.objects.create(id_flowers=flowers,img=item)
+        #     images.save()
         return flowers
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name',instance.name)
