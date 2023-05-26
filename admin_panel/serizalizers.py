5bf0136378ec91cd2586867913bdef46c02a7143
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-05-26 15:38:52 +0500

    Flowers Images Multiples

--- admin_panel/serizalizers.py
+++ admin_panel/serizalizers.py
@@ -69,13 +69,13 @@ class FlowersBaseAllSerializers(serializers.ModelSerializer):
 class FlowersBaseCruderializers(serializers.ModelSerializer):
     # id_category = CategoriyaAllSerializers(read_only=True)
     # id_sub_category = SubCategoriyaAllSerializers(read_only=True)
-    # img2= FlowersImagesSer(many=True, read_only = True)
+    flowers= FlowersImagesSer(many=True, read_only = True)
     img = serializers.ListField(
         child = serializers.ImageField(max_length = 1000000, allow_empty_file = False, use_url = False),
         write_only=True)
     class Meta:
         model = Flowers
-        fields = ['id','name','cotent','rank','price','like','iye','id_category','id_sub_category','img']
+        fields = ['id','name','cotent','rank','price','like','iye','id_category','id_sub_category','img','flowers']
     def create(self, validated_data):
         img = validated_data.pop('img')
         flowers = Flowers.objects.create(**validated_data)
