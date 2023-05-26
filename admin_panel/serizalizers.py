3dd4ca6bb2f2dccb1966ed0019592dcb10cf12ae
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-05-26 17:34:50 +0500

    sdsd

--- admin_panel/serizalizers.py
+++ admin_panel/serizalizers.py
@@ -75,7 +75,7 @@ class FlowersBaseCruderializers(serializers.ModelSerializer):
         write_only=True)
     class Meta:
         model = Flowers
-        fields = ['id','name','cotent','rank','price','like','iye','id_category','id_sub_category','img','flowers']
+        fields = ['id','name','cotent','rank','price','like','iye','id_category','id_sub_category','img']
     def create(self, validated_data):
         img = validated_data.pop('img')
         flowers = Flowers.objects.create(**validated_data)
