aaa9d7d2e285f4712a2c2dd296a8a708cc08998e
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-06-01 14:55:59 +0500

    Site Views All

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.views.static import serve
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework_simplejwt import views as jwt_views
@@ -11,7 +12,7 @@ urlpatterns = [
     path('api/base/',include('admin_panel.urls')),
     path('api/sayts/',include('sayts.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
 
     
